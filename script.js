0dd403755ae99f47383dc16b54b77b847c97a350
Janne <EXT-Janne.Huotari@tulli.fi>
2019-02-04 09:11:24 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+var screen = {
+  w: window.innerWidth,
+  h: window.innerHeight
+}
+canvas.width = screen.w
+canvas.height = screen.h
